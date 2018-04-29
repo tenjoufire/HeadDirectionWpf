2c310e79f6ba51ba075cce408bdf3aca4b2125b7
Kaisei Tsujimoto <tenjoufire7@gmail.com>
2018-04-29 16:40:25 +0900

    動画再生部分作成

--- HeadDirectionWpf/MainWindow.xaml.cs
+++ HeadDirectionWpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +22,147 @@ namespace HeadDirectionWpf
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        //ストーリーボード
+        Storyboard storyboard = null;
+
+        string movieFileName = "";
+
+        //再生中かどうか
+        bool isPlaying = false;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void PlayMovie()
+        {
+            //メディアタイムラインを作成
+            MediaTimeline mediaTimeline = new MediaTimeline(new Uri(movieFileName));
+            mediaTimeline.CurrentTimeInvalidated += new EventHandler(mediaTimeline_CurrentTimeInvalidated);
+            Storyboard.SetTargetName(mediaTimeline, MoviePlayer.Name);
+
+            //ストーリーボードを作成・開始
+            storyboard = new Storyboard();
+            storyboard.Children.Add(mediaTimeline);
+            storyboard.Begin(this, true);
+
+            //コントロールの変更
+            SliderTime.IsEnabled = true;
+            Play.IsEnabled = true;
+            Stop.IsEnabled = true;
+
+            isPlaying = true;
+
+        }
+
+        private void StopMovie()
+        {
+            //ストーリーボードの停止
+            storyboard.Stop(this);
+            storyboard.Children.Clear();
+            storyboard = null;
+
+            //コントロールの変更
+            SliderTime.Value = 0.0;
+            SliderTime.IsEnabled = false;
+            Stop.IsEnabled = false;
+
+            isPlaying = false;
+
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            //何も再生されていないときは新しく再生を始める
+            if (storyboard == null)
+            {
+                PlayMovie();
+            }
+            else //すでに再生されている場合は，一時停止か再び再生か
+            {
+                if (isPlaying)
+                {
+                    storyboard.Pause(this);
+                }
+                else
+                {
+                    storyboard.Resume(this);
+                }
+                Play.Content = isPlaying ? "Pause" : "Play";
+                isPlaying = !isPlaying;
+            }
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            //ファイルを開くダイアログボックスを表示
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "動画ファイル(*.avi;*.wmv;*.mpg;*.mpeg;*.mp4;*.mkv;*.m2ts;*.flv)|*.avi;*.wmv;*.mpg;*.mpeg;*.mp4;*.mkv;*.m2ts;*.flv";
+            if (openFileDialog.ShowDialog() != true)
+                return;
+
+            //ファイル名を保存する
+            movieFileName = openFileDialog.FileName;
+            //開いたファイルを表示
+            FileName.Text = movieFileName.Split("\\"[0]).LastOrDefault();
+
+            if (storyboard != null)
+            {
+                StopMovie();
+            }
+
+            PlayMovie();
+        }
+
+        private void mediaTimeline_CurrentTimeInvalidated(object sender, EventArgs e)
+        {
+            if (storyboard != null)
+            {
+                SliderTime.Value = MoviePlayer.Clock.CurrentTime.Value.TotalMilliseconds;
+
+                if (MoviePlayer.NaturalDuration.HasTimeSpan && MoviePlayer.Clock.CurrentTime.Value.TotalMilliseconds == MoviePlayer.NaturalDuration.TimeSpan.TotalMilliseconds)
+                {
+                    StopMovie();
+                }
+            }
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            StopMovie();
+        }
+
+        private void MoviePlayer_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            SliderTime.Maximum = MoviePlayer.NaturalDuration.TimeSpan.TotalMilliseconds;
+        }
+
+        private void SliderTime_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        {
+            if (isPlaying)
+            {
+                storyboard.Pause(this);
+            }
+        }
+
+        private void SliderTime_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            //動画をシークする
+            storyboard.Seek(this, new TimeSpan((long)Math.Floor(SliderTime.Value * TimeSpan.TicksPerMillisecond)), TimeSeekOrigin.BeginTime);
+
+            if (isPlaying)
+            {
+                storyboard.Resume(this);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+
     }
 }
